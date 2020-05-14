b1386fc8bfa5fd1ff7689c1037c62ec49017901e
Ryan Brandt <ryan.brandt@biomeme.com>
2020-05-14 00:12:33 -0400

    initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const App: React.FunctionComponent<Props> = (props: Props): JSX.Element => {
+  return <div className="App">App go here : )</div>;
+};
+
+export default App;
