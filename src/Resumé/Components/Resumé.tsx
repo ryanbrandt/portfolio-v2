ba896b9df2909b24369c290e5c055f6a068200b1
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2020-06-11 22:27:53 -0400

    basic resumé layout

--- src/Resumé/Components/Resumé.tsx
+++ src/Resumé/Components/Resumé.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Divider, Button } from "handsome-ui";
+import { Divider, Row, Column } from "handsome-ui";
 
 interface Props {}
 
@@ -10,6 +10,22 @@ const Resumé: React.FunctionComponent<Props> = (): JSX.Element => {
       <div>
         <h1 className="aligned_text">Resumé</h1>
         <Divider />
+        <div style={{ width: "80vw" }}>
+          <Row>
+            <Column className="resume_column">
+              <h3 className="resume_header">Development Experience</h3>
+              <Divider />
+            </Column>
+            <Column className="resume_column">
+              <h3 className="resume_header">Education</h3>
+              <Divider />
+            </Column>
+            <Column className="resume_column">
+              <h3 className="resume_header">Tools & Technology</h3>
+              <Divider />
+            </Column>
+          </Row>
+        </div>
       </div>
     </div>
   );
