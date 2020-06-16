423940eb7ec51b19ec15d30de03af17c80995041
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2020-06-15 21:22:21 -0400

    contact form WIP

--- src/Contact/Components/Contact.tsx
+++ src/Contact/Components/Contact.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import { Divider } from "handsome-ui";
 
+import ContactForm from "../Subcomponents/ContactForm";
+
 interface Props {}
 
 const Contact: React.FunctionComponent<Props> = (): JSX.Element => {
   return (
     <div className="fadeable-content flex_center_col">
       <div>
-        <h1 className="aligned_text">Contact</h1>
+        <h1 className="aligned_text">Start the Conversation</h1>
         <Divider />
+        <ContactForm />
       </div>
     </div>
   );
---
+++ src/Contact/Subcomponents/ContactForm.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+import { Input, Button } from "handsome-ui";
+
+import { recaptchaKey } from "../../utils/secrets";
+
+interface Props {}
+
+interface ContactForm {
+  [key: string]: any;
+  name: string;
+  email: string;
+  content: string;
+}
+
+const initialState: ContactForm = {
+  name: "",
+  email: "",
+  content: "",
+};
+
+const ContactForm: React.FunctionComponent<Props> = (props: Props) => {
+  const [fields, setFields] = useState(initialState);
+  const [captchaValidated, setCaptchaValidated] = useState(false);
+
+  const _formIsValid = (): boolean => {
+    let allFieldsComplete = true;
+    for (const key in fields) {
+      if (fields[key].length < 1) {
+        allFieldsComplete = false;
+        break;
+      }
+    }
+
+    return captchaValidated && allFieldsComplete;
+  };
+
+  const _renderRecaptcha = (): React.ReactNode => {
+    return (
+      <div className="contact_recaptcha">
+        <ReCAPTCHA
+          sitekey={recaptchaKey}
+          onChange={(token: string | null) =>
+            setCaptchaValidated(token ? true : false)
+          }
+        />
+      </div>
+    );
+  };
+
+  const _renderSubmitButton = (): React.ReactNode => {
+    const disabled = !_formIsValid();
+
+    return (
+      <div className="flex_center_col contact_submit">
+        <Button
+          title="Send Message"
+          onClick={() => null}
+          disabled={disabled}
+          inverting
+          round
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className="contact_form">
+      <Input
+        label="Name"
+        placeholder="Your Full Name"
+        value={fields.name}
+        containerClassName="contact_input"
+        onChange={(value: string) => setFields({ ...fields, name: value })}
+      />
+      <Input
+        label="Email"
+        placeholder="Your Email Address"
+        value={fields.email}
+        containerClassName="contact_input"
+        onChange={(value: string) => setFields({ ...fields, email: value })}
+      />
+      {_renderRecaptcha()}
+      {_renderSubmitButton()}
+    </div>
+  );
+};
+
+export default ContactForm;
