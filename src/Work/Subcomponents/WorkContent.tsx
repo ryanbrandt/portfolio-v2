ab502f1fb2063f0e62c696d56a6b22e1c49c5201
ryan.brandt@biomeme.com <rnb52@scarletmail.rutgers.edu>
2020-11-01 23:18:33 -0500

    lib update + source and deploy work links

--- src/Work/Subcomponents/WorkContent.tsx
+++ src/Work/Subcomponents/WorkContent.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 
-import { Modal, AppContext } from "handsome-ui";
+import { Modal, AppContext, Row } from "handsome-ui";
 
 import { RootState } from "../../store/rootReducer";
 import { getActiveWorkItem } from "../selectors";
+import { safeOpenWindow } from "../../utils/helpers";
 
 interface Props {
   open: boolean;
@@ -18,24 +19,54 @@ interface StateProps {
 const WorkContent = (props: Props & StateProps) => {
   const { open, activeItem, onClose } = props;
 
+  const _renderLinks = () => {
+    return (
+      <Fragment>
+        {activeItem.source && (
+          <Fragment>
+            <div
+              className="work-link"
+              onClick={() => safeOpenWindow(activeItem.source)}
+            >
+              Source
+            </div>
+          </Fragment>
+        )}
+        {activeItem.deploy && (
+          <Fragment>
+            <div
+              className="work-link"
+              onClick={() => safeOpenWindow(activeItem.deploy)}
+            >
+              Deployed Project
+            </div>
+          </Fragment>
+        )}
+      </Fragment>
+    );
+  };
+
   if (activeItem) {
     return (
       <AppContext.Consumer>
-        {isMobile => (
-          <Modal heading={activeItem.name} open={open} onClose={onClose}> 
-          <div className="work_content-container">
-            <div className="flex_center_col">
-              <img
-                  className={isMobile ? "work-img-mobile": "work-img"}
+        {(isMobile) => (
+          <Modal heading={activeItem.name} open={open} onClose={onClose}>
+            <div className="work_content-container">
+              <div className="flex_center_col">
+                <img
+                  className={isMobile ? "work-img-mobile" : "work-img"}
                   src="project-placeholder.jpg"
                   alt="project-placeholder.jpg"
                 />
-            </div>
-            <div className="work_content-datestring">{activeItem.datestring}</div>
+              </div>
+              <div className="work_content-datestring">
+                {activeItem.datestring}
+                {_renderLinks()}
+              </div>
               <p>{activeItem.description}</p>
             </div>
           </Modal>
-          )}
+        )}
       </AppContext.Consumer>
     );
   }
