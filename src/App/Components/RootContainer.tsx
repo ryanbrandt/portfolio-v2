b105894c41a088a7c42befe86e8df2d7b258653e
ryan.brandt@biomeme.com <rnb52@scarletmail.rutgers.edu>
2021-01-01 12:00:07 -0500

    lib updates.. again

--- src/App/Components/RootContainer.tsx
+++ src/App/Components/RootContainer.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Router } from "react-router";
 
-import { AppContainer, LoadingOverlay } from "handsome-ui";
+import { AppContainer, Header, LoadingOverlay } from "handsome-ui";
 
 import { getContentLoading } from "../selectors";
 import routes, { history } from "../../routes";
@@ -14,109 +14,31 @@ interface StateProps {
   contentLoading: boolean;
 }
 
-interface MenuOption {
-  [key: string]: any;
-  name: string;
-  route: string;
-}
-
-interface MenuItem extends MenuOption {
-  active: boolean;
-}
-
-const MENU_OPTIONS = [
-  { name: "Home", route: "/" },
-  { name: "Work", route: "/work" },
-  { name: "Resumé", route: "/resume" },
-  { name: "Contact", route: "/contact" },
-  { name: "Blog", route: "/blog" },
-];
-
-const initialState: Array<MenuItem> = [
-  { name: "Home", route: "/", active: true },
-  { name: "Work", route: "/work", active: false },
-  { name: "Resumé", route: "/resume", active: false },
-  { name: "Contact", route: "/contact", active: false },
-  { name: "Blog", route: "/blog", active: false },
-];
-
 const RootContainer: React.FunctionComponent<Props & StateProps> = (
   props: Props & StateProps
 ): JSX.Element => {
   useEffect(() => {
-    const { location } = history;
-    const { pathname } = location;
-    _handlePathChange(pathname);
-
-    history.listen((location, action) => {
-      const { pathname } = location;
-
-      _handlePathChange(pathname);
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0);
     });
+
+    return () => unlisten();
   }, []);
 
-  const [menuOptions, setMenuOptions] = useState<Array<MenuItem>>(initialState);
-
-  const _handlePathChange = (activePath: string): void => {
-    window.scrollTo(0, 0);
-    setMenuOptions(
-      MENU_OPTIONS.map((option) => {
-        if (option.route === activePath) {
-          return {
-            ...option,
-            active: true,
-          };
-        }
-        return { ...option, active: false };
-      })
-    );
-  };
-
-  const _handleMobileClick = (item: string): void => {
-    const menuOption = MENU_OPTIONS.find((option) => option.name === item);
-    if (menuOption) {
-      history.push(menuOption.route);
-    } else {
-      history.push("/");
-    }
-  };
-
-  const _getMobileMenu = (): Array<string> => {
-    return menuOptions
-      .filter((option: MenuItem) => !option.active)
-      .map((option: MenuItem) => option.name);
-  };
-
-  const _renderHeaderMenu = (): React.ReactNode => {
-    return (
-      <div className="flex_space_row app_menu">
-        {menuOptions.map((option: MenuItem) => (
-          <span
-            key={option.name}
-            className={
-              option.active
-                ? "app_menu_active medium_text"
-                : "app_menu_option medium_text"
-            }
-            onClick={() => {
-              history.push(option.route);
-            }}
-          >
-            {option.name}
-          </span>
-        ))}
-      </div>
-    );
-  };
-
   const { contentLoading } = props;
+
+  const headerOptions = [
+    { name: "Home", action: () => history.push("/") },
+    { name: "Work", action: () => history.push("/work") },
+    { name: "Resumé", action: () => history.push("/resume") },
+    { name: "Contact", action: () => history.push("/contact") },
+    { name: "Blog", action: () => history.push("/blog") },
+  ];
 
   return (
     <AppContainer
       className="root_container"
-      mobileMenu={_getMobileMenu()}
-      headerMenu={_renderHeaderMenu()}
-      onMobileClick={_handleMobileClick}
+      header={<Header options={headerOptions} />}
     >
       <Router history={history}>{routes}</Router>
       <LoadingOverlay show={contentLoading} fade />
